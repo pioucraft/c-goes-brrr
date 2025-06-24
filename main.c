7990a70f758ae77523b172b90bd7d26f8e229f7a
Nathan Gasser <hello@gougoule.ch>
2025-06-24 11:50:49 +0200

    idk

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 
 int main() {
-    printf("Denis Popov le Père Noël\n");
+    double first = 0.000000001;
+    double second = 0.000000002;
+    double sum = first + second;
+    printf("%.50lf\n", second);
     return 0;
 }
